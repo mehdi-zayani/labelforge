8a71f3adaba7aac9fddb53bccc481830b191be7c
Mehdi Zayani <mehdi.zayani.dev@gmail.com>
2026-05-12 13:06:25 +0100

    feat(github): centralize Octokit client initialization

--- src/github/client.ts
+++ src/github/client.ts
@@ -1,6 +1,12 @@
 import { Octokit } from "@octokit/rest";
 import { getGitHubToken } from "../env/env.js";
 
+const token = getGitHubToken();
+
+if (!token) {
+  throw new Error("[GITHUB] Missing GitHub token in environment");
+}
+
 export const octokit: Octokit = new Octokit({
-  auth: getGitHubToken()
+  auth: token
 });
