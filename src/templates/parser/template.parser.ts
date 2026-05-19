cdfa4624d7d03491d1df070de1abf6c8df7f940c
Mehdi Zayani <mehdi.zayani.dev@gmail.com>
2026-05-19 18:26:50 +0100

    feat(template): add YAML schema parser and validation layer

---
+++ src/templates/parser/template.parser.ts
@@ -0,0 +1,8 @@
+import fs from "fs";
+import yaml from "js-yaml";
+import type { GitHubLabelTemplate } from "../types/template.types.js";
+
+export function parseTemplate(filePath: string): GitHubLabelTemplate {
+  const file = fs.readFileSync(filePath, "utf-8");
+  return yaml.load(file) as GitHubLabelTemplate;
+}
